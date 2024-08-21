b84fa8bb0db138e83901f97e22186b541842028f
Rodrigo Tejero <rodrigo.tejero@chimp.mx>
2024-08-21 09:54:01 -0600

    ✨ Update API key and add new function for conversation
    - Updated API key to use GROQ API key
    - Added a new function for conversation with Luna
    - Adjusted system prompts and link generation for blinks

--- services/blinks.service.js
+++ services/blinks.service.js
@@ -2,7 +2,14 @@ import 'dotenv/config';
 import OpenAI from 'openai';
 import * as fs from 'fs';
 
-const openai = new OpenAI(process.env.OPENAI_API_KEY);
+//const openai = new OpenAI(process.env.OPENAI_API_KEY);
+const openai = new OpenAI({
+	apiKey: process.env.GROQ_API_KEY,
+	baseURL: 'https://api.groq.com/openai/v1',
+});
+
+//const model = 'gpt-4o';
+const model = 'llama3-groq-70b-8192-tool-use-preview';
 
 class BlinksService {
 
@@ -52,6 +59,7 @@ class BlinksService {
 								description: 'The URL to call the function. in this case its https://appapi.lunadefi.ai/blinks/transfer-sol',
 							},
 						},
+						required: ['to', 'amount', 'url'],
 					},
 				},
 			},
@@ -109,42 +117,71 @@ class BlinksService {
 					},
 				},
 			},
+			// conversation
+			{
+				type: 'function',
+				function: {
+					name: 'conversation',
+					description: 'Have a conversation with Luna.',
+					parameters: {
+						type: 'object',
+						properties: {
+							prompt: {
+								type: 'string',
+								description: 'The exact prompt sent by the user.',
+							},
+						},
+					},
+				},
+			},
 		];
 
 		const messages = [
 			{
 				role: 'system',
-				content: 'You are Luna, a Solana crypto assistant. Your main job is to help users find the best Solana actions for their needs. You have access to the following tools: memo, transfer sol, swap, and stake bonk',
+				content: 'You are Luna, a Solana crypto assistant. ' +
+					'Your main job is to help users find the best Solana actions for their needs. ' +
+					'You have access to the following tools: memo, transfer sol, swap, and stake bonk. ' +
+					'If no method is selected, you should have a conversation with the user.',
 			},
 			{ role: 'user', content: prompt },
 		];
 
 		const response = await openai.chat.completions.create({
-			model: 'gpt-4o',
+			model: model,
 			messages: messages,
 			tools: tools,
 			tool_choice: 'required',
 		});
 
 		const selectedFunction = response.choices[0].message.tool_calls[0].function;
+		console.log(selectedFunction);
 
 		const selectedFunctionArgs = JSON.parse(selectedFunction.arguments);
-		const dialectLink = `https://app.lunadefi.ai/blinks/${ selectedFunctionArgs.url }`;
+		const dialectLink = `https://dial.to/?action=solana-action:${ selectedFunctionArgs.url }`;
+
+		const conversationPrompt = 'You are Luna, a Solana crypto assistant. ' +
+			'Your personality is funny but professional, a little sassy maybe.' +
+			'You always answer in the user\'s language, and you are always helpful. ' +
+			'Your main job is to help the user with blinks. ';
+
+		const blinkSystemPrompt = 'Offer the user the link to the blink and an explanation on how to use it.' +
+			'This is the link you should offer: ' + dialectLink +
+			'These are the args to consider: ' + selectedFunction.arguments;
 
 		const messages2 = [
 			{
 				role: 'system',
-				content: 'You are Luna, a Solana crypto assistant.' +
-					'Your personality is funny but professional, a little sassy maybe. Your main job is to help the user with blinks. ' +
-					'You always answer in the user\'s language, and you are always helpful. ' +
-					'Offer the user the link to the blink and an explanation on how to use it.' +
-					'This is the link you should offer: ' + dialectLink +
-					'These are the args to consider: ' + selectedFunction.arguments,
+				content: selectedFunction.name === 'conversation' ? conversationPrompt : blinkSystemPrompt,
 			},
 		];
 
+		if(selectedFunction.name === 'conversation') {
+			messages2.push({ role: 'user', content: prompt });
+		}
+
 		const response2 = await openai.chat.completions.create({
-			model: 'gpt-4o',
+			model: model,
 			messages: messages2,
 		});
 
